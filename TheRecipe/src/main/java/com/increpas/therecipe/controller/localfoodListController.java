82fca505339c72fe43ef2171396cf0201374da0c
2-16 <2-16@192.168.0.40>
2017-11-24 18:24:25 +0900

    지역 리스트 컨트롤러 수정 중

--- TheRecipe/src/main/java/com/increpas/therecipe/controller/localfoodListController.java
+++ TheRecipe/src/main/java/com/increpas/therecipe/controller/localfoodListController.java
@@ -1,5 +1,31 @@
 package com.increpas.therecipe.controller;
 
-public class localfoodListController {
+import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.increpas.therecipe.service.LocalService;
+import com.increpas.therecipe.vo.FoodVO;
+
+@Controller
+public class localfoodListController {
+		Logger logger = LoggerFactory.getLogger(getClass());
+		
+		@Autowired
+		LocalService localService;
+		
+		@RequestMapping(value = "/foodList.do", method = RequestMethod.GET)
+		public String foodList(@Valid @ModelAttribute("icmd") FoodVO fvo, Model model){
+			
+			model.addAttribute("food", localService.selectList());
+			
+			return "";
+		}
 }
