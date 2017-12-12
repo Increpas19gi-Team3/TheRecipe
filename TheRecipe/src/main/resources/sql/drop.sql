1e7fcf82c63ec0367793bdb4ddaec6808a70aa47
2-16 <2-16@2-16-HP>
2017-12-12 17:32:03 +0900

    다시 커밋

--- TheRecipe/src/main/resources/sql/drop.sql
+++ TheRecipe/src/main/resources/sql/drop.sql
@@ -3,6 +3,7 @@ drop SEQUENCE tr_food_seq;
 drop SEQUENCE tr_event_seq;
 drop table tr_foodcode;
 drop table tr_order;
+drop table tr_orderdetail;
 drop table tr_basket;
 drop table tr_review;
 drop table tr_member;
